507d5ed5d1101b4e363b813162ce2f3658786533
Nick Forsberg <seventy6.com@gmail.com>
2014-04-16 22:39:10 +0100

    tweaked the styling on the ui

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -28,62 +28,27 @@ angular.module('yikeesApp')
         }, true);
 
         $scope.$watch('myDate', function() {
-            $scope.todo = '';
+          
+            //this would reset the todo
+            //$scope.todo = '';
+
             localStorageService.add('todo', $scope.todo);
             localStorageService.add('myDate', $scope.myDate);
             //$scope.updateClock();
             setUpDate();
         }, true);
 
-        console.log('$scope.myDate', $scope.myDate, '$scope.todo', $scope.todo);
-        // $scope.addTodo = function () {
-        //   $scope.todo = $scope.todo;
-        //   //$scope.todos.push($scope.todo + '_' + $scope.myDate);
-        //   //$scope.todo = '';
-        // };
-        // $scope.removeTodo = function (index) {
-        //       $scope.todos.splice(index, 1);
-        // };
-        // for (var i = 0; i < 10; i++) {
-        //     console.log(var n = i; String('00' + n).slice(-2);)
-        // };
-        //create and set the clock
-        $scope.timeDaysFirst = 0;
-        $scope.timeDaysSecond = 0;
-        $scope.timeHoursFirst = 0;
-        $scope.timeHoursSecond = 0;
-        $scope.timeMinutesFirst = 0;
-        $scope.timeMinutesSecond = 0;                                
-        $scope.timeSecondsFirst = 0;
-        $scope.timeSecondsSecond = 0;
-
+        
         function setUpDate() {
 
             //destroy old clock
             window.clearInterval(timerId);
-
-            console.log(new Date($scope.myDate));//.getFullYear(), $scope.myDate.getMonth(), $scope.myDate.getDay());
-
+            
             timerId =
                 countdown(
                     new Date($scope.myDate),
                     function(ts) {
                         
-                        //console.log('ts', ts);
-                        
-                        // if (!$scope.$$phase) {
-                        //     $scope.$apply(function() {
-                        //         $scope.timeDaysFirst = String('00' + ts.days).slice(-2).charAt(0);
-                        //         $scope.timeDaysSecond = String(ts.days).slice(-1);
-                        //         $scope.timeHoursFirst = String('0' + ts.hours).slice(-2).charAt(0);
-                        //         $scope.timeHoursSecond = String(ts.hours).slice(-1);
-                        //         $scope.timeMinutesFirst = String('0' + ts.minutes).slice(-2).charAt(0);
-                        //         $scope.timeMinutesSecond = String(ts.minutes).slice(-1);
-                        //         $scope.timeSecondsFirst = String('0' + ts.seconds).slice(-2).charAt(0);
-                        //         $scope.timeSecondsSecond = String(ts.seconds).slice(-1);
-                        //     });
-                        // }
-                        //$('#clock').html('<ul class="flip "><li data-digit="0" class="flip-clock-active"><a href="#" class="flip-clock-before"><div class="down flip-clock-before"><div class="shadow"></div><div class="inn flip-clock-before">' + ts.days + '</div></div></a></li></ul><ul class="flip "><li data-digit="0" class="flip-clock-active"><a href="#" class="flip-clock-before"><div class="down flip-clock-before"><div class="shadow"></div><div class="inn flip-clock-before">' + ts.hours + '</div></div></a></li></ul><ul class="flip"><li class="flip-clock-active">' + ts.minutes + '</li></ul>' + '<ul class="flip"><li class="flip-clock-active">' + ts.seconds + '</li></ul>');
                         $('#clock').html(ts.toHTML("em"));
                     },
                     //countdown.MILLENNIA |
